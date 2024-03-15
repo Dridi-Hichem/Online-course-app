c66bf4cbe2584d8e9fd8cd95dd3bb94b3f88549e
Hichem Dridi <dridi.hichem18@gmail.com>
2024-03-15 20:16:24 +0100

    Add questions related models

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -1,34 +1,28 @@
 import sys
-from django.utils.timezone import now
+
 try:
     from django.db import models
+    from django.conf import settings
+    from django.utils.timezone import now
 except Exception:
     print("There was an error loading django modules. Do you have django installed?")
     sys.exit()
-
-from django.conf import settings
-import uuid
 
 
 # Instructor model
 class Instructor(models.Model):
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
     full_time = models.BooleanField(default=True)
     total_learners = models.IntegerField()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.user.username
 
 
 # Learner model
 class Learner(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
     STUDENT = 'student'
     DEVELOPER = 'developer'
     DATA_SCIENTIST = 'data_scientist'
@@ -39,33 +33,40 @@ class Learner(models.Model):
         (DATA_SCIENTIST, 'Data Scientist'),
         (DATABASE_ADMIN, 'Database Admin')
     ]
+
     occupation = models.CharField(
         null=False,
         max_length=20,
         choices=OCCUPATION_CHOICES,
         default=STUDENT
     )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,  on_delete=models.CASCADE
+    )
     social_link = models.URLField(max_length=200)
 
-    def __str__(self):
-        return self.user.username + "," + \
-               self.occupation
+    def __str__(self) -> str:
+        return f"{self.user.username}, {self.occupation}"
 
 
 # Course model
 class Course(models.Model):
-    name = models.CharField(null=False, max_length=30, default='online course')
+    name = models.CharField(
+        null=False, max_length=30, default='online course'
+    )
     image = models.ImageField(upload_to='course_images/')
     description = models.CharField(max_length=1000)
     pub_date = models.DateField(null=True)
     instructors = models.ManyToManyField(Instructor)
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Enrollment')
+    users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, through='Enrollment'
+    )
     total_enrollment = models.IntegerField(default=0)
     is_enrolled = False
 
-    def __str__(self):
-        return "Name: " + self.name + "," + \
-               "Description: " + self.description
+    def __str__(self) -> str:
+        return f"Name: {self.name}, Description: {self.description}"
 
 
 # Lesson model
@@ -95,9 +96,32 @@ class Enrollment(models.Model):
     rating = models.FloatField(default=5.0)
 
 
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    grade = models.IntegerField(default=50)
+
+    def __str__(self) -> str:
+        return f"Question: {self.content}"
+
+    def is_get_score(self, selected_ids) -> bool:
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choicr_set.filter(
+            is_correct=True, id__in=selected_ids
+        ).count()
+    
+        return all_answers == selected_correct
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+class Submission(models.Model):
+   enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+   choices = models.ManyToManyField(Choice)
